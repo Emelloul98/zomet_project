d6e8ecf6454d1f292e6128ff3d70e28f4c8983bb
emanuel malloul <gnbutknkuk@gmail.com>
2024-06-11 20:28:25 +0300

    expo-router bug fix and home page added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,85 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, PixelRatio } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { normalize, normalizeHeight,height } from '../styles/globalDimension';
-
+import React from "react";
+import {StyleSheet,Text,View,Image,TouchableOpacity,Dimensions,PixelRatio,} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { normalize, normalizeHeight, height } from "../styles/globalDimension";
+import { AntDesign } from '@expo/vector-icons';
 type navProps = {
   nav: any;
-  settingsOrSched: string;
-  iconeType: string;
-  styleType: string;
-}
+  settingsIcon: string;
+  backIcon: string;
+  whereToBack: string;
+};
 export default function Header(props: navProps) {
-
-  const moveToSettingsOrSched = () => {
-    props.nav.replace(props.settingsOrSched);
-  }
-
 
   return (
     <View style={styles.header}>
-        <View style={styles.header_icons}>
-          <View style={styles.left_icons}>
-            <Ionicons name="wifi-outline" style={styles.wifi_icon} />
-            <TouchableOpacity onPress={moveToSettingsOrSched}>
-              {props.settingsOrSched === 'Settings' ? 
-              <Ionicons name="settings-outline" style={styles.settings_icon} /> : 
-              <Ionicons name="calendar" style={styles.settings_icon} /> 
-              }
+      <View style={styles.header_icons}>
+        <View style={styles.left_icons}>
+          {props.backIcon === "Back" && (
+            <TouchableOpacity onPress={()=> props.nav.navigate(props.whereToBack)}>
+              <AntDesign name="left" size={24} style={styles.headers_icon}/>
             </TouchableOpacity>
-          </View>
-          <Image 
-            style={styles.zomet_logo}
-            source={require('../assets/images/zomet_icon.png')}
-            resizeMode="contain"
-          />
+          )}
+          <Ionicons name="wifi-outline" style={styles.headers_icon} />
+          {props.settingsIcon === "Settings" && (
+            <TouchableOpacity onPress={()=> props.nav.navigate("Settings")}>
+              <Ionicons name="settings-outline" style={styles.headers_icon} />
+            </TouchableOpacity>
+          )}
         </View>
-        <Text style={styles.application_title}>שעון אסטרונומי צומת</Text>
+        <Image
+          style={styles.zomet_logo}
+          source={require("../assets/images/zomet_icon.png")}
+          resizeMode="contain"
+        />
+      </View>
+      <Text style={styles.application_title}>אפליקציית שעון אסטרונומי </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    height: height*0.15,
+    height: height * 0.15,
   },
   header_icons: {
     flex: 1,
-    flexDirection: 'row',
-    width: '100%',
+    flexDirection: "row",
+    width: "100%",
   },
   left_icons: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf: 'flex-start',
-    
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "flex-start",
   },
   application_title: {
-    fontFamily: 'Arial',
-    textAlign: 'center',
-    color: '#231dd3',
+    fontFamily: "Arial",
+    textAlign: "center",
+    color: "#231dd3",
     fontSize: normalize(27),
-    marginLeft:'5%', 
+    marginLeft: "5%",
   },
   zomet_logo: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     marginRight: normalize(10),
-    width: normalize(80), 
-    height: normalizeHeight(70), 
+    width: normalize(80),
+    height: normalizeHeight(70),
   },
-  wifi_icon: {
+  headers_icon: {
     marginLeft: normalize(20),
-    color: '#231dd3',
+    color: "#231dd3",
     fontSize: normalize(20),
     marginTop: normalizeHeight(25),
   },
-
-  settings_icon: {
-    marginLeft: normalize(20),
-    color: '#231dd3',
-    fontSize: normalize(20),
-    marginTop: normalizeHeight(25),
-  },
- 
 });
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,39 +1,75 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
-import Header from '../components/Header';
-import { globalStyles } from '@/styles/global';
-import HebrewDate  from '@/components/HebrewDate';
-import MainArea from "../components/MainArea";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Header from "../components/Header";
+import { globalStyles } from "@/styles/global";
 
 export default function Home({ navigation }) {
-  const sunrise="6:00";
-  const sunset="18:00";
-  const country="ישראל";
-  return (
-      <View style={globalStyles.homeMainView}>
-        <Header nav={navigation} settingsOrSched='Settings' iconeType="settings-outline"/>
-        <View style={globalStyles.bodyContainer}>
-            <View style={styles.texts}>
-              <Text style={styles.text_element}>ארץ {country}</Text>
-              <HebrewDate textStyle={styles.text_element} />
-              <Text style={styles.text_element}>זריחה: {sunrise}</Text>
-              <Text style={styles.text_element}>שקיעה:{sunset}</Text>
-            </View>
-            <MainArea/>  
-        </View>
 
+  const [connection, setConnection] = useState(false);
+
+  return (
+    <View style={globalStyles.screenContainer}>
+      <Header nav={navigation} />
+      <View style={styles.container}>
+        {connection && (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("EditTable")}
+          >
+            <Text style={styles.buttonText}>ערוך טבלת שעון</Text>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert("חיפוש שעון טרם מומש")}
+        >
+          <Text style={styles.buttonText}>חפש שעון חדש</Text>
+        </TouchableOpacity>
+        {!connection && (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("EditTable")}
+            >
+            <Text style={styles.buttonText}>ערוך טבלה offline</Text>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setConnection(!connection)}
+        >
+          <Text style={styles.buttonText}>שנה מצב חיבור</Text>
+        </TouchableOpacity>
       </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  texts:{
-    marginTop:'4%',
+  texts: {
+    marginTop: "4%",
   },
   text_element: {
-    color:'#231dd3',
-    alignSelf:'flex-end',
-    marginRight:'2%',
-    fontWeight:'bold'
+    color: "#231dd3",
+    alignSelf: "flex-end",
+    marginRight: "2%",
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: "80%",
+    padding: 20,
+    backgroundColor: "#6200EE",
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
