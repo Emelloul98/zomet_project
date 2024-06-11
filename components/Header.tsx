28f24b1d4b79c797151fd61ec683226b2447519c
emanuel malloul <gnbutknkuk@gmail.com>
2024-06-11 21:19:17 +0300

    header fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,15 +24,16 @@ export default function Header(props: navProps) {
           {props.settingsIcon === "Settings" && (
             <TouchableOpacity onPress={()=> props.nav.navigate("Settings")}>
               <Ionicons name="settings-outline" style={styles.headers_icon} />
-            </TouchableOpacity>)}
-          </View>
-          <Image 
-            style={styles.zomet_logo}
-            source={require('../assets/images/zomet_icon.png')}
-            resizeMode="contain"
-          />
+            </TouchableOpacity>
+          )}
         </View>
-        <Text style={styles.application_title}>שעון אסטרונומי צומת</Text>
+        <Image
+          style={styles.zomet_logo}
+          source={require("../assets/images/zomet_icon.png")}
+          resizeMode="contain"
+        />
+      </View>
+      <Text style={styles.application_title}>שעון אסטרונומי צומת</Text>
     </View>
   );
 }
